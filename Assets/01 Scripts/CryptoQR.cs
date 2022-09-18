7b3958bee011cbf3843a29cedef30a36682bbba8
Hades000 <lsw463@naver.com>
2022-09-19 01:24:33 +0900

    Feat: EncryptSHA Function

--- Assets/01 Scripts/CryptoQR.cs
+++ Assets/01 Scripts/CryptoQR.cs
@@ -1,18 +1,24 @@
+using System.Text;
 using System.Collections;
 using System.Collections.Generic;
+using System.Security.Cryptography;
 using UnityEngine;
 
-public class CryptoQR : MonoBehaviour
+public class CryptoQR
 {
-    // Start is called before the first frame update
-    void Start()
+    /// <summary>
+    /// SHA-256 알고리즘으로 데이터 암호화
+    /// </summary>
+    /// <param name="data">암호화할 데이터</param>
+    /// <returns></returns>
+    private static string EncryptSHA(string data)
     {
-        
-    }
+        var bytes = Encoding.UTF8.GetBytes(data);
+        var hash = new SHA256CryptoServiceProvider().ComputeHash(bytes);
+        var encryptedData = new StringBuilder();
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+        foreach(var h in hash) encryptedData.AppendFormat("{0:x2}",h);
+
+        return encryptedData.ToString();
     }
 }
